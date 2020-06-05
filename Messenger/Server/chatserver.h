b86effba5848c3d98362e966ad352d8b733dcf86
Kremenevskiy <vkremenevskiy@gmail.com>
2020-06-05 17:28:56 +0300

    Added some coments to Server classes

--- Messenger/Server/chatserver.h
+++ Messenger/Server/chatserver.h
@@ -10,7 +10,7 @@
 
 #include "worker.h"
 
-
+// Основной класс управления сервером
 class ChatServer : public QTcpServer
 {
 	Q_OBJECT
@@ -18,28 +18,36 @@ class ChatServer : public QTcpServer
 public:
 	explicit ChatServer(QObject *parent = nullptr);
 
+	// остановка сервера
 	void StopServer();
 
 signals:
+	// сигнал при отправке сообщения в лог
 	void logMessage(const QString &msg);
 
 public slots:
+	// набор действий если пользователь отключился
 	void UserDisconnected(Worker *sender);
 
 private slots:
+	// Если к нам пришло сообещение в формате josn, обрабатыаем его
 	void JsonReceived(Worker *sender, const QJsonObject jsonObj);
+	// Трансляция нашего jsonObject на всех пользователей
 	void Broadcast(const QJsonObject jsonObj, Worker *exclude);
 
 protected:
+	// Обработка входящих подключений
 	void incomingConnection(qintptr handle) override;
 
 private:
+	// Если пришел json от залогиненного пользователя
 	void JsonFromLoggedIn(Worker *sender, const QJsonObject jsonObj);
+	// Если пришел   json от нового пользователя
 	void JsonFromLoggedOut(Worker *sender, const QJsonObject jsonObj);
-
+	// Передаем jsonObject получателю
 	void SendJson(Worker *receiver, const QJsonObject jsonObj);
 
-
+	// Храним список подключенных пользователей
 	QList <Worker *> clients;
 	//QThreadPool *threadPool;
 
--- Messenger/Server/worker.h
+++ Messenger/Server/worker.h
@@ -13,31 +13,41 @@
 #include <QJsonParseError>
 
 
+// Класс на стороне сервера для взаимодейсвтия с пользователем
 class Worker : public QObject
 {
 	Q_OBJECT
 public:
 	explicit Worker(QObject *parent = nullptr);
 
+	// устанавливаем дискриптор для клиента
 	bool setSocketDescriptor(int handle);
+	// Функции для установления имени клиента
 	void SetUserName(QString userName);
+	// Получение имени
 	QString UserName();
-
+	// Отправка сообщения на клиентский сокет
 	void SendJson(const QJsonObject jsonObj);
 
 
 signals:
+	// Для отправки сообщения в лог
 	void logMessage(const QString &msg);
+	// При отключении пользователя
 	void disconnectedFromServer();
+	// Транслируем сообщение в класс ChatServer, чтобы там его обработать
 	void jsonReceived(Worker *sender, const QJsonObject jsonObj);
 
 public slots:
+	// Закрваем соединение на сокете
 	void DisconnectFromServer();
+	// Если у нас имеются новые данные, считываем их, проверяем на json
 	void onReadyRead();
 
 private:
 
 	QString userName;
+	// Храним сокет
 	QTcpSocket *serverSocket;
 	int socketDescriptor;
 
